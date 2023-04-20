feaf44966e433c6744460ab388a33dc2945a3287
Gabriel Plassard <gabriel.plassard@gmail.com>
2023-04-20 23:52:38 +0200

    fix(build): regenerate projen & create main (#2)
    * fix(build): regenerate projen & create main
    
    * fix(build): force rename cargo.toml -> Cargo.toml
    
    * fix(build): specify version

--- .projenrc.js
+++ .projenrc.js
@@ -1,11 +1,12 @@
 const { RustProject } = require('@gplassard/projen-extensions');
+const package = require('./package.json');
 
 const project = new RustProject({
    name: 'rust-project-playground',
    cargo: {
       package: {
          authors: ["user <user@mail.com>"],
-         version: 'version',
+         version: package.version,
          edition: "2021",
       },
       dependencies: {
